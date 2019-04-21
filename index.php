a5e7971e6aea2b58b58bc284d0c4034a6a7932e5
dudu9999 <ecaetanocorrea@gmail.com>
2019-04-20 21:10:21 -0300

    funcoes strlen PHP 2

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+//$nome = "Eduardo Correa";
+//echo substr($nome, 0, 6); //Motra X Quantidade de caracteres
+//echo strtolower("OLA, COMO VAI?");
+//cho strtoupper("ola, como vai?");
+//$msg = strip_tags("<h1>Bem-vindo</h1>, acesse o <a href=''><b>link</b></a>", "<a> <b>");
+//echo $msg;
+//$comida = "Banana, Maça, Pera, Uva";
+//$alt = str_ireplace("banana", "Morango", $comida);
+//echo $alt;
+//$nome = "Eduardo Caetano Correa";
+//$ex = explode(" ", $nome);
+//echo $ex[0];
+$nome = "Fulano";
+
+if (strlen($nome) <= 10) {
+    echo "Válido";
+} else {
+    echo "Inválido";
+}
